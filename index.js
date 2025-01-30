11ccb6875de987a250cf16a6b9a8baec0a7fa5a2
rima-das2004 <rima983131@gmail.com>
2025-01-30 20:05:17 +0530

    edited

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const listing=require("./models/listing.js")
-
+const ejs=require("ejs");
+app.set("view engine","ejs")
+const path=require("path")
+app.set("views",path.join(__dirname,"views"));
+const ejsMate=require("ejs-mate");
+app.use(express.urlencoded({extended:true}));
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/GypsyVerse');
   
@@ -23,22 +28,46 @@ app.get("/",(req,res)=>{
     res.send("working");
 })
 
-app.get("/test", (req,res)=>{
-    let SampleList= new listing({
-        title:"Villa",
-        description:"new villa",
-        price:50000,
-        location:"Goa",
-        country:"India",
+// app.get("/test", (req,res)=>{
+//     let SampleList= new listing({
+//         title:"Villa",
+//         description:"new villa",
+//         price:50000,
+//         location:"Goa",
+//         country:"India",
         
-    })
+//     })
     
-  SampleList.save().then((res)=>{
+//   SampleList.save().then((res)=>{
+//     console.log(res)
+//   })
+//   .catch((err)=>{
+//     console.log(err)
+//   })
+//     res.send("OK")
+// })
+app.get("/listing",async(req,res)=>{
+  const listData= await listing.find({});
+  res.render("listing/index.ejs",{listData})
+})
+app.get("/listing/create",(req,res)=>{
+  res.render("listing/create.ejs")
+})
+app.post("/listing",async (req,res)=>{
+  let list=req.body.listing;
+  console.log(list)
+  let newSample= new listing(list)
+  newSample.save().then((res)=>{
     console.log(res)
   })
   .catch((err)=>{
-    console.log(err)
+    console.log(err);
   })
-    res.send("OK")
+  res.redirect("/listing");
+})
+app.get("/listing/:id",async (req,res)=>{
+  let {id}=req.params;
+  let DataById=await listing.findById(id);
+  res.render("listing/show.ejs",{DataById});
 })
 
